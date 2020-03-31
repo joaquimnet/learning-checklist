d2a72694cba55bdef8c33bfbc8c6e51c5b1530c6
Joaquim <joaquimmy@yahoo.com>
2020-03-31 15:04:52 -0300

    Turn site into a vue app

--- src/js/app.js
+++ src/js/app.js
@@ -1,74 +1,62 @@
-import o from 'domil';
 import { fetchPage, fetchListing } from './page-fetcher';
+import Vue from 'vue/dist/vue.esm.browser';
 
-const contentHolder = o('.content');
+new Vue({
+  el: '#app',
+  data: {
+    pageListing: {},
+    pages: {},
+    currentPage: '<h1>Click on a link to navigate.</h1>',
+    showUpButton: false,
+  },
+  computed: {
+    sidebarItems() {
+      return Object.keys(this.pageListing).map(category => {
+        return {category, topics: this.pageListing[category]};
+      });
+    },
+  },
+  methods: {
+    linkClicked(event) {
+      event.preventDefault();
 
-// console.log(contentEl);
+      const pageName = event.target.attributes.href.value;
+      if (!pageName) return;
 
-// const path = location.pathname.substr(1).split('/');
+      if (this.pages[pageName]) {
+        this.currentPage = this.pages[pageName];
+        return;
+      }
 
-// console.log(location.pathname.split('/'));
-// console.log(path);
-
-const pages = {};
-
-function remToPixels(rem) {    
-  return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
-}
-
-const linkClickedEvtHandler = event => {
-  event.preventDefault();
-
-  const pageName = event.target.attributes.href.value;
-  if (!pageName) return;
-
-  if (pages[pageName]) {
-    contentHolder.innerHTML = pages[pageName];
-    return;
-  }
-
-  fetchPage(pageName).then(res => {
-    console.log(`Fetching ${pageName}...`);
-    pages[pageName] = res;
-    contentHolder.innerHTML = res;
-  });
-};
-
-const sidebarGroupTemplate = (category, topics) => {
-  return `<div class="sidebar-group">
-<span class="sidebar-header">
-  ${category}
-</span>
-<ul>
-  ${topics
-    .map(topic => {
-      return `<li><a href="${topic.path}">${topic.pageName}</a></li>`;
-    })
-    .join('\n')}
-</ul>
-</div>`;
-};
-
-fetchListing().then(pageListing => {
-  const sidebarContent = Object.keys(pageListing)
-    .map(category => {
-      return sidebarGroupTemplate(category, pageListing[category]);
-    })
-    .join('\n');
-  o('.sidebar').innerHTML = sidebarContent;
-  o('a').forEach(el => el.addEventListener('click', linkClickedEvtHandler));
-});
-
-o('.fixed-overside').addEventListener('click', e => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-});
-
-document.addEventListener('scroll', e => {
-  if (window.scrollY >= remToPixels(4)) {
-    // is visible
-    o.css(o('.fixed-overside'), { zIndex: 1000000000000000 });
-  } else {
-    // is not visible
-    o.css(o('.fixed-overside'), { zIndex: -1 });
-  }
+      fetchPage(pageName).then(res => {
+        console.log(`Fetching ${pageName}...`);
+        this.pages[pageName] = res;
+        this.currentPage = res;
+      });
+    },
+    clicked() {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    },
+    handleScroll() {
+      if (window.scrollY >= this.remToPixels(4)) {
+        // is visible
+        this.showUpButton = true;
+      } else {
+        // is not visible
+        this.showUpButton = false;
+      }
+    },
+    remToPixels(rem) {
+      return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+    }
+  },
+  created() {
+    window.addEventListener('scroll', this.handleScroll);
+    fetchListing().then(pageListing => {
+      this.pageListing = pageListing;
+    });
+  },
+  destroyed() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
 });
